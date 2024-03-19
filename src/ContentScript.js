b01b1d2c6590d6088035767b9fe67d22b85d93cf
abhijitbanerjee05 <itsabhijitbanerjee@gmail.com>
2024-03-19 16:33:27 -0400

    modal position change for better interaction

--- src/ContentScript.js
+++ src/ContentScript.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MicrophoneIcon from './MicrophoneIcon';
+import './MicrophoneIcon.css'
 
 function injectMicrophoneIcon() {
   const jiraContainer = document.getElementsByClassName('css-njhh6c');//css-vtikxo
-  const container = document.createElement('div')
+  const container = document.createElement('div');
+  container.classList.add('center-div');
   jiraContainer[0].appendChild(container);
   ReactDOM.render(<MicrophoneIcon />, container);
 }
