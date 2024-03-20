c7db6ade8f064b31d177717acf534158b26ad79f
SladeJustinFerrao <sladejustincredible@gmail.com>
2024-03-19 22:50:18 -0400

    Resolved bug where neqw tasks werent getting added

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,10 @@ import { faMicrophone } from '@fortawesome/free-solid-svg-icons';
 function App() {
 
   let keywords = ['create', 'insert', 'add', 'implement', 'generate', 'compose', 'form', 'formulate', 'setup', 'update', 'set', 'change', 'alter', 'modify', 'edit', 'correct', 'make', 'move', 'transition', "delete", "remove", "scratch", "cross"];
-  const [temp, setTemp] = useState(-1);
+  const [keywordIndex, setKeywordIndex] = useState(-1);
+  const [flag, setFlag] = useState(true);
+
+  const [temp, setTemp] = useState('');
 
   const [textInput, setTextInput] = useState('');
 
@@ -74,22 +77,23 @@ function App() {
 
   useEffect(() => {
     getEnvVars();
-    if (temp >= 0 && temp <= 8) {
+    if (keywordIndex >= 0 && keywordIndex <= 8) {
       addTask();
-    } else if (temp > 8 && temp <= 16) {
+    } else if (keywordIndex > 8 && keywordIndex <= 16) {
       updateTask();
-    } else if (temp > 16 && temp <= 18) {
+    } else if (keywordIndex > 16 && keywordIndex <= 18) {
       transitionTask();
-    } else if (temp > 18) {
+    } else if (keywordIndex > 18) {
       deleteTask();
     }
-  }, [temp]);
+  }, [flag]);
 
   const matchesSequence = (inputString) => {
     const match = keywords.findIndex(seq => inputString.toLowerCase().includes(seq.toLowerCase()));
 
     if (match >= 0 && match <= 20) {
-      setTemp(match);
+      setKeywordIndex(match);
+      setFlag(!flag);
     }
   }
 
@@ -135,7 +139,7 @@ function App() {
       const data = await response.json();
       const stat = await response.status;
       if (stat === 201) {
-        speak(`Task with key ${data.key} is Created`);
+        speak(`Task with key ${data.key} is Created having summary ${summary}`);
       } else {
         speak(`The Task was not created`);
       }
@@ -148,7 +152,6 @@ function App() {
     let updateElementKeywords = ['label', 'summary', 'description'];
 
     let key = 'No Key';
-    let board = 'HCI board';
     let label = 'No Label';
     let summary = 'no summary';
     let description = 'no desc';
