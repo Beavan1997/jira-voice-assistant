39a0cd38bfe73eb5a1114fabc5810c880b20c09a
smridhi29 <smridhi68@gmail.com>
2024-03-20 00:53:33 -0400

    updated json logic

--- src/App.jsx
+++ src/App.jsx
@@ -214,6 +214,23 @@ function App() {
       description = '';
     }
 
+    const descrip = {
+        content: [
+          {
+            content: [
+              {
+                text: "REPSTR",
+                type: "text"
+              }
+            ],
+            type: "paragraph"
+          }
+        ],
+        type: "doc",
+        version: 1
+    }
+    const lab = []
+
     const fields = {
       project: {
         key: "HCI"
@@ -228,11 +245,13 @@ function App() {
     }
 
     if (label !== undefined) {
-      fields.label = label;
+      lab.push(label);
+      fields.labels = lab;
     }
 
     if (description !== undefined) {
-      fields.description = description;
+      descrip.content[0].content[0].text = description;
+      fields.description = descrip;
     }
 
     const bodyData = JSON.stringify({
